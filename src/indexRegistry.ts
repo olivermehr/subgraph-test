62262456e6705bce2a80b0e511a0d53fcf42abbe
olivermehr <68856567+olivermehr@users.noreply.github.com>
2023-12-13 15:43:53 +0000

    Merge subgraph.yaml files and configure mustache

--- src/indexRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { feePool } from "../generated/templates"
-import {
-  SetFeePool as SetFeePoolEvent
-} from "../generated/templates/indexRegistry/indexRegistry"
-
-export function handleSetFeePool(event: SetFeePoolEvent): void {
-  feePool.create(event.params.feePool)
-}
-
