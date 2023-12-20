a0f441a1edc60f0801212fd353e95792a721b900
olivermehr <68856567+olivermehr@users.noreply.github.com>
2023-12-20 14:04:21 +0000

    Improve extensibility of yaml template, make v1 work with new structure (inc USV),properly decode A160U96 structs

--- src/v1/IndexRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { FeePool } from "../../generated/templates"
-import {
-  SetFeePool as SetFeePoolEvent
-} from "../../generated/templates/IndexRegistry/IndexRegistry"
-
-export function handleSetFeePool(event: SetFeePoolEvent): void {
-  FeePool.create(event.params.feePool)
-}
-
