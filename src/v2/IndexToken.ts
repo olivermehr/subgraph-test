4eaa6d18c7d19a4b123bd74813bef5e4cc79f11a
olivermehr <68856567+olivermehr@users.noreply.github.com>
2024-07-03 16:55:55 +0100

    Add logs

--- src/v2/IndexToken.ts
+++ src/v2/IndexToken.ts
@@ -21,22 +21,28 @@ export function handleWithdraw(event: WithdrawEvent): void {
     let reserveAssetEntity = createOrLoadIndexAssetEntity(event.address, reserveAsset, indexEntity.chainID)
     let scalar = new BigDecimal(BigInt.fromI32(10).pow(u8(reserveAssetEntity.decimals)))
     let amount = new BigDecimal(event.params.reserve).div(scalar)
+    log.debug("Withdrawn amount = {}, reserve balance before = {}, withdrawn k",[amount.toString(),reserveAssetEntity.balance.toString(),event.params.k.toString()])
     reserveAssetEntity.balance = reserveAssetEntity.balance.minus(amount)
+    log.debug("Reserve balance after subtracting withdrawn reserve {}",[reserveAssetEntity.balance.toString()])
     reserveAssetEntity.save()
     let k = indexEntity.k!
     if (event.params.k > BigInt.zero()) {
         let assetScalar = BigDecimal.fromString("1").minus(new BigDecimal(event.params.k).div(new BigDecimal(k)))
+        log.debug("Asset scalar {}",[assetScalar.toString()])
         let indexAssets = indexEntity.assets
         for (let i = 0; i < indexAssets.length; i++) {
             let chainIDToAssetMappingEntity = loadChainIDToAssetMappingEntity(indexAssets[i])
             let chainIDAssetArray = chainIDToAssetMappingEntity.assets
             for (let y = 0; y < chainIDAssetArray.length; y++) {
                 let indexAssetEntity = loadIndexAssetEntity(chainIDAssetArray[y])
+                log.debug("Asset address is {} and balance before scaling {}",[indexAssetEntity.asset.toHexString(),indexAssetEntity.balance.toString()])
                 indexAssetEntity.balance = indexAssetEntity.balance.times(assetScalar)
+                log.debug("Asset address is {} and balance before scaling {}",[indexAssetEntity.asset.toHexString(),indexAssetEntity.balance.toString()])
                 indexAssetEntity.save()
             }
         }
         indexEntity.k = k.minus(event.params.k)
+        
         indexEntity.save()
     }
     saveHistoricalData(event.address, event.block.timestamp)
