5be6e57ae01b379f0681aa901246e620bc6a2101
olivermehr <68856567+olivermehr@users.noreply.github.com>
2024-07-09 15:32:03 +0100

    Improve AUM fee calculation

--- src/v1/FeePool.ts
+++ src/v1/FeePool.ts
@@ -2,75 +2,103 @@ import {
   SetAUMScaledPerSecondsRate as SetAUMScaledPerSecondsRateEvent,
   SetBurningFeeInBP as SetBurningFeeInBPEvent,
   SetMintingFeeInBP as SetMintingFeeInBPEvent,
-} from "../../generated/templates/FeePool/FeePool"
-import { createOrLoadIndexEntity } from "../EntityCreation"
-import { BigDecimal, BigInt, Bytes, log } from "@graphprotocol/graph-ts"
+} from "../../generated/templates/FeePool/FeePool";
+import { createOrLoadIndexEntity } from "../EntityCreation";
+import { BigDecimal, BigInt, Bytes } from "@graphprotocol/graph-ts";
 
+const RATE_SCALE_BASE = BigDecimal.fromString("1000000000000000000000000000");
+const WAD = BigDecimal.fromString("1000000000000000000");
+const EPS = BigDecimal.fromString("0.000000001");
 
-export function handleSetAUMScaledPerSecondsRate(
-  event: SetAUMScaledPerSecondsRateEvent
-): void {
-  convertAUMFeeRate(event.params.index, event.params.AUMScaledPerSecondsRate)
+const MOD = BigDecimal.fromString("10000000");
+
+const ONE = BigDecimal.fromString("1");
+const TWO = BigDecimal.fromString("2");
+
+function exp(x: BigDecimal): BigDecimal {
+  let result = ONE;
+  let term = ONE;
+
+  const iters = BigDecimal.fromString("20");
+  for (let i = ONE; i.lt(iters); i = i.plus(ONE)) {
+    term = term.times(x).div(i);
+    result = result.plus(term);
+    if (term.lt(EPS)) break;
+  }
+
+  return result;
+}
+
+function ln(x: BigDecimal): BigDecimal {
+  if (x.le(BigDecimal.zero()))
+    throw new Error("ln(x) is only defined for x > 0");
+
+  let y = x.minus(ONE).div(x.plus(ONE));
+  let y2 = y.times(y);
+  let result = BigDecimal.zero();
+  let term = y;
+
+  const iters = BigDecimal.fromString("50");
+  for (let i = ONE; i.lt(iters); i = i.plus(TWO)) {
+    result = result.plus(term.div(i));
+    term = term.times(y2);
+    if (term.lt(EPS)) break;
+  }
+
+  return result.times(TWO);
+}
+
+export function calculateAUMRate(secondsPerYear: BigDecimal, AUMDilutionPerSecond: BigDecimal): BigDecimal {
+  const x = AUMDilutionPerSecond.div(RATE_SCALE_BASE);
+
+  const res = exp(ln(x).times(secondsPerYear)).minus(ONE);
+
+  const resBigInt = BigInt.fromString(res.times(WAD).truncate(0).toString());
+  const mod = resBigInt.mod(BigInt.fromString(MOD.toString()));
+
+  if (mod.ge(BigInt.fromString(MOD.toString()).div(BigInt.fromI32(2)))) {
+    return BigDecimal.fromString(
+      resBigInt.plus(BigInt.fromString(MOD.toString())).toString(),
+    )
+      .div(WAD)
+      .truncate(4);
+  }
+
+  return BigDecimal.fromString(resBigInt.toString()).div(WAD).truncate(4);
 }
 
 export function convertAUMFeeRate(index: Bytes, aumFee: BigInt): void {
-  let indexEntity = createOrLoadIndexEntity(index)
-  let scaledPerSecondRate = new BigDecimal(aumFee)
-  let constant = BigDecimal.fromString('1000000000000000000000000000')
-  let seconds_per_year: BigDecimal
-  if (indexEntity.version == "v2") {
-    seconds_per_year = new BigDecimal(BigInt.fromI32(31556952))
-  }
-  else {
-    seconds_per_year = new BigDecimal(BigInt.fromI32(31536000))
-  }
-  let q = constant.div(scaledPerSecondRate);
-  let one = new BigDecimal(BigInt.fromI32(1))
-  let two = new BigDecimal(BigInt.fromI32(2))
-  let scalar = BigInt.fromI64(1000000000000000000).toBigDecimal()
-  let roundingPrecision = BigInt.fromString(scalar.times(BigDecimal.fromString('0.0005')).toString())
+  let indexEntity = createOrLoadIndexEntity(index);
+  let scaledPerSecondRate = new BigDecimal(aumFee);
 
-  // e = 1 - C / s = 1 - q
-  let e = one.minus(q)
+  const secondsPerYear = BigDecimal.fromString(indexEntity.version == "v2" ? "31556952" : "31536000");
 
-  // p1 = e*(N+1)/2
-  let p1 = e.times(seconds_per_year.plus(one)).div(two)
+  indexEntity.aumFee = calculateAUMRate(secondsPerYear, scaledPerSecondRate);
+  indexEntity.save();
+}
 
-  // e*e*(N+1)*(N+2)/6
-  let p2 = e
-    .times(e)
-    .times(seconds_per_year.plus(one))
-    .times(seconds_per_year.plus(two))
-    .div(new BigDecimal(BigInt.fromI32(6)));
-
-  // x4 = N * e * (1 - p1 + p2)
-  let x4 = seconds_per_year.times(e).times(one.minus(p1).plus(p2)).times(scalar).truncate(0);
-  let x4_big = BigInt.fromString(x4.toString())
-  let mod = x4_big.mod(roundingPrecision)
-  let wholeNumber = x4_big.minus(mod)
-  let fee: BigDecimal
-  if (mod >= roundingPrecision.div(BigInt.fromI32(2))) {
-    fee = new BigDecimal(wholeNumber.plus(roundingPrecision)).div(scalar)
-  }
-  else {
-    fee = new BigDecimal(wholeNumber).div(scalar)
-  }
-  indexEntity.aumFee = fee
-  indexEntity.save()
+export function handleSetAUMScaledPerSecondsRate(
+  event: SetAUMScaledPerSecondsRateEvent,
+): void {
+  convertAUMFeeRate(event.params.index, event.params.AUMScaledPerSecondsRate);
 }
 
 export function handleSetBurningFeeInBP(event: SetBurningFeeInBPEvent): void {
-  let indexEntity = createOrLoadIndexEntity(event.params.index)
-  let scalar = new BigDecimal(BigInt.fromI32(10000))
-  let fee = new BigDecimal(BigInt.fromI32(event.params.burningFeeInPB)).div(scalar)
-  indexEntity.redemptionFee = fee
-  indexEntity.save()
+  let indexEntity = createOrLoadIndexEntity(event.params.index);
+  let scalar = new BigDecimal(BigInt.fromI32(10000));
+  let fee = new BigDecimal(BigInt.fromI32(event.params.burningFeeInPB)).div(
+    scalar,
+  );
+  indexEntity.redemptionFee = fee;
+  indexEntity.save();
 }
 
 export function handleSetMintingFeeInBP(event: SetMintingFeeInBPEvent): void {
-  let indexEntity = createOrLoadIndexEntity(event.params.index)
-  let scalar = new BigDecimal(BigInt.fromI32(10000))
-  let fee = new BigDecimal(BigInt.fromI32(event.params.mintingFeeInBP)).div(scalar)
-  indexEntity.mintingFee = fee
-  indexEntity.save()
+  let indexEntity = createOrLoadIndexEntity(event.params.index);
+  let scalar = new BigDecimal(BigInt.fromI32(10000));
+  let fee = new BigDecimal(BigInt.fromI32(event.params.mintingFeeInBP)).div(
+    scalar,
+  );
+  indexEntity.mintingFee = fee;
+  indexEntity.save();
 }
